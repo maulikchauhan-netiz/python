1c1c6a6ebdf1b63bb6eb9032da1c0e6bb6e4e3b7
maulik chauhan <maulik.rajput0001@gmail.com>
2026-06-25 18:57:49 +0530

    python code

---
+++ 8.py
@@ -0,0 +1,51 @@
+# arithmetic operator
+
+x=10;
+y=5;
+
+print("arithmetic operator")
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
+print(" ")
+
+# comparision operator
+
+print("comaprision operator")
+print(x==y)
+print(x<=y)
+print(x>=y)
+print(x<y)
+print(x>y)
+print(" ")
+
+# logical operator
+
+print("logical operator")
+print(x<y and x>y)
+print(x>y and x>y)
+print(x>y or x<y)
+print(not(x>y))
+print(" ")
+
+# assignment operator
+
+print("assignment operator")
+a=2;
+b=3;
+
+a=a+2
+a+=2
+
+a=a-2
+a-=2
+
+a=a*10
+a*=10
+
+a=a/2
+a/=2
+
