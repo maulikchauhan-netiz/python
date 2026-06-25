5e532c388d8005717472f38859520dc58e50afed
maulik chauhan <maulik.rajput0001@gmail.com>
2026-06-25 19:04:43 +0530

    python code

---
+++ 9.py
@@ -0,0 +1,10 @@
+# take input as celsius and print its equivalent in fahrenheit and kelvin 
+
+c=int(input("enter the celsius"))
+print("celsius is : ",c)
+
+f=(c * 9/5) + 32
+print("fahrenheit is : ",f)
+
+k=c + 273.15
+print("kelvin is : ",k)
