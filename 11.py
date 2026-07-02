0a2da326a7b335ee41283b33690e267523d8c084
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-02 19:46:47 +0530

    py code

--- 11.py
+++ 11.py
@@ -12,3 +12,16 @@ print(str1[9])
 
 firsthalf=str1[0:5]
 print(firsthalf)
+
+str2="pizzaa"
+print(str2[2:5])
+print(str2[4:6])
+
+#slicing practice question
+food=input("enter the food item")
+mid=len(food)//2
+output1=food[mid-1:mid+2]
+print(output1)
+
+output2=food[-2:]
+print(output2)
