0031a759812a22eea2a539adf15e4c2e4c76383f
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-02 19:31:08 +0530

    python code

--- 11.py
+++ 11.py
@@ -2,3 +2,13 @@ str3="maulik"
 print(str3[0])
 print(str3[5])
 print(len(str3))
+
+#slicing
+str1="gulabjamun"
+print(str1[0:5])
+print(str1[:10])
+print(str1[:9])
+print(str1[9])
+
+firsthalf=str1[0:5]
+print(firsthalf)
