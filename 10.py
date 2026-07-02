3a477776b60ef9fa568b4494ee1b94688ab89a7c
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-02 19:19:17 +0530

    python code

---
+++ 10.py
@@ -0,0 +1,12 @@
+str1='hello'
+str2="maulik"
+
+print(str1)
+print(str2)
+
+#string concatination
+print(str1 + " " +str2)
+ 
+#length of string
+print(len(str1)) 
+
---
+++ 11.py
@@ -0,0 +1,4 @@
+str3="maulik"
+print(str3[0])
+print(str3[5])
+print(len(str3))
