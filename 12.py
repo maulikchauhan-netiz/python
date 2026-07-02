72bfa846a6f4876de9a8c76fb90c964d95dc097a
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-02 19:58:55 +0530

    python code

--- 12.py
+++ 12.py
@@ -7,3 +7,12 @@ print(str1.title())
 print(str1.find("l"))
 print(str1.replace("ma","MA"))
 print(str1.count("k"))
+
+
+#pratice question
+
+str=input("enter the string ")
+lower=(str.upper())
+replace=(str.replace(" ","_"))
+print(lower)
+print(replace)
