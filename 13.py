ee7f5288742dab4b28fc63bedd6e085f013a1319
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-03 20:10:02 +0530

    python code

--- 13.py
+++ 13.py
@@ -31,3 +31,14 @@ else:
     print("fail")            
 
 
+#pratice question
+
+num=int(input("enter the number"))
+if(num>0):
+    print("positive")
+elif(num==0):
+    print("zero")
+elif(num<0):
+    print("negative")
+else:   
+    print("invalid")         
