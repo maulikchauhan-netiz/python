a5dca3ac69e3e5c611de2c037f47ab993509de7f
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-03 19:04:56 +0530

    python code

---
+++ 13.py
@@ -0,0 +1,22 @@
+#conditinal statements
+
+
+#if else
+
+marks=99
+if(marks>=90):
+    print("grade A")
+else:
+    print("grade B")
+
+#elif
+
+age=18
+if(age>=18):
+    print("you are eligible")
+else:
+    print("you are not eligible")
+        
+
+
+
