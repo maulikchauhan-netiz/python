1510494557fb31cd15e51e701740e3bada231cf5
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-03 20:39:47 +0530

    python code

---
+++ list.py
@@ -0,0 +1,27 @@
+#list practice python
+
+cars=["volvo","bmw","audi","tata"]
+print(len(cars))
+print(cars[0])
+print(cars[1:5])
+print(cars[-2])
+print(cars[:3])
+
+#list methods
+
+marks=[99,100,12,50]
+print(marks)
+marks[1]=98
+print(marks)
+marks.append(500)
+print(marks)
+marks.insert(1,80)
+print(marks)
+marks.remove(500)
+print(marks)
+marks.pop(2)
+print(marks)
+marks.sort()
+print(marks)
+marks.reverse()
+print(marks)
