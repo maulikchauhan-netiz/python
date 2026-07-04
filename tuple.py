e06b36e3c8a2212eabca5b117770b5d7ce564fe5
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-04 20:27:37 +0530

    python code

---
+++ tuple.py
@@ -0,0 +1,10 @@
+#tuple practice
+mytuple=("bmw","volvo","audi")
+
+#tuples are immutable
+
+emptytuple=()
+print(type(emptytuple))
+print(type(mytuple))
+print(mytuple.count("bmw"))
+print(mytuple.index("volvo"))
