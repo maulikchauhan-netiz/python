daea390c033e6522cdea874f29fec1ad6e1d33c3
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-04 20:34:15 +0530

    python code

--- tuple.py
+++ tuple.py
@@ -1,6 +1,6 @@
 #tuple practice
 mytuple=("bmw","volvo","audi")
-
+tuple=(99,88,77,55)
 #tuples are immutable
 
 emptytuple=()
@@ -8,3 +8,7 @@ print(type(emptytuple))
 print(type(mytuple))
 print(mytuple.count("bmw"))
 print(mytuple.index("volvo"))
+print(len(mytuple))
+print(max(tuple))
+print(min(tuple))
+
