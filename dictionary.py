b4d8fdae5e92c70de26c16240da44720ccf1342f
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-04 21:06:55 +0530

    python code

--- dictionary.py
+++ dictionary.py
@@ -7,3 +7,20 @@ car={
 }
 print(type(car))
 print(car["name"])
+car["name"]= "audi"
+print(car)
+car["price"]=1000000
+print(car)
+car.pop("price")
+print(car)
+print(car.keys())
+print(car.values())
+
+#practice
+marks={
+    "maths":94,
+    "science":80,
+    "english":70
+
+}
+print(marks)
