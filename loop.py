2d28a46f4538afb7b1cfdf819e78f6cc2d142f21
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-09 22:24:46 +0530

    python code

---
+++ loop.py
@@ -0,0 +1,19 @@
+#while loop
+num=1
+while num <= 5:
+    print ("Maulik")
+    num=num+1
+print("now we are out of the loop")
+#practice question
+
+num=1
+while num<=10:
+    print(num)
+    num+=1
+#practice question 2
+print("===============")
+num=10
+while num>=1:
+    print(num)
+    num-=1    
+print("===============")
