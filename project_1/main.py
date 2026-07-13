13fa1d302071d2ab6a6d3610a35cc23954bd4953
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-13 23:02:51 +0530

    python code

--- project_1/main.py
+++ project_1/main.py
@@ -1,1 +1,27 @@
 #expense traker
+from datetime import datetime
+
+# Get current hour
+hour = datetime.now().hour
+
+if 5 <= hour < 12:
+    print("----------🌅 Good Morning! MAULIK----------")
+elif 12 <= hour < 17:
+    print("----------☀️ Good Afternoon! MAULIK----------")
+elif 17 <= hour < 21:
+    print("----------🌇 Good Evening! MAULIK----------")
+else:
+    print("🌙 Good Night! MAULIK")
+expenses=[]
+print("Welcome to expenses traker Maulik")
+
+
+while True:
+        print("----------MENU----------")
+        print("1.Add expense")
+        print("2.view expense")
+        print("3.view total spending")
+        print("4.exit")
+
+if choice==1:
+     print        
