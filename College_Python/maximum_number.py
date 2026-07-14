5f863265492811321ed64fa8ac9f6ab5f50ef5be
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-14 08:01:26 +0530

    python code

---
+++ College_Python/maximum_number.py
@@ -0,0 +1,16 @@
+a=int(input("enter the a : "))
+b=int(input("enter the b : "))
+c=int(input("enter the c : "))
+
+if a>b and a>c:
+    print("a is big")
+elif b>c and b>a:
+    print("b is big")
+elif c>b and c>a:
+    print("c is big")
+else:
+    print("invalid number")        
+
+
+
+
