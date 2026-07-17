34ae353677034c616611835393ff926a03ceda93
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-17 19:54:20 +0530

    python code

--- function.py
+++ function.py
@@ -3,3 +3,28 @@ def add(a,b):
     return a+b;
 print("the addition of the",add(20,30))
       
+def sum():
+    a=20;
+    b=20;
+    z=a+b;
+    print("sum is ",z)
+sum()    
+
+
+#practice question
+def welcome_message():
+    print("welcome to python programming")
+welcome_message()
+welcome_message()
+welcome_message()
+
+#pratice 2
+def inspire():
+    print("you can do it maulik")
+inspire()    
+
+#practice 3
+def details(name,age):
+    print(f"my namae is the {name} and my age is the {age}");
+details("maulik",20)
+
